e975a40437e75328a4a0711cbe855bec7ab6984f
Simon <tsmiller7373@gmail.com>
2024-08-27 13:16:45 -0400

    Connected BE to login/signup flow

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+const LOCAL = true;
+
+axios.defaults.baseURL = LOCAL ? 'http://localhost:3001' : '/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
